6664e324cfda8702602e481bdffd899aec56ed84
Ian Lewis <33504743+wellthathappened@users.noreply.github.com>
2026-09-11 10:24:03 -0700

    Almost finished movement code for 3D

--- Lewis_3D/Assets/Scripts/PlayerController.cs
+++ Lewis_3D/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float jumpHeight = 10f;
+
+    PlayerInput playerInput;
+    Rigidbody rb;
+
+    Vector2 moveInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Initializing component data
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        // Setting up new move Vector
+        moveInput = new Vector2();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 tempMovement = rb.linearVelocity;
+
+        rb.linearVelocity = (moveInput.x * speed) + (moveInput.y * speed) + (moveInput.z * speed);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
