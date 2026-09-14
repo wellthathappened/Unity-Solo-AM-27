02591635290a4fefb01618f306e6de44f94c5abd
Ian Lewis <33504743+wellthathappened@users.noreply.github.com>
2026-09-14 09:57:15 -0700

    Prototypical Movement + Jumping for 2D & 3D

--- Lewis_2D/Assets/Scripts/PlayerController.cs
+++ Lewis_2D/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float jumpHeight = 10f;
+    public float jumpDetectDistance = .1f;
+
+    PlayerInput playerInput;
+    Rigidbody2D rb;
+
+    Ray2D jumpRay;
+    Vector2 moveInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Initializing component data
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+
+        // Setting up new move Vector
+        moveInput = Vector2.zero;
+
+        jumpRay = new Ray2D(transform.position, -transform.up);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        jumpRay.origin = transform.position;
+        jumpRay.direction = -transform.up;
+
+        Vector2 tempMove = rb.linearVelocity;
+
+        tempMove.x = moveInput.x * speed;
+
+        rb.linearVelocity = tempMove;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput.x = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump()
+    {
+        if (Physics2D.Raycast(jumpRay.origin, jumpRay.direction, jumpDetectDistance))
+            rb.AddForceY(jumpHeight,ForceMode2D.Impulse);
     }
 }
--- Lewis_3D/Assets/Scripts/PlayerController.cs
+++ Lewis_3D/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,12 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpHeight = 10f;
+    public float jumpDetectDistance = 1.1f;
 
     PlayerInput playerInput;
     Rigidbody rb;
 
+    Ray jumpRay;
     Vector2 moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,19 +21,35 @@ public class PlayerController : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
 
         // Setting up new move Vector
-        moveInput = new Vector2();
+        moveInput = Vector2.zero;
+
+        jumpRay = new Ray(transform.position, -transform.up);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 tempMovement = rb.linearVelocity;
+        jumpRay.origin = transform.position;
+        jumpRay.direction = -transform.up;
 
-        rb.linearVelocity = (moveInput.x * speed) + (moveInput.y * speed) + (moveInput.z * speed);
+        Vector3 tempMove = rb.linearVelocity;
+
+        tempMove.x = (moveInput.x * speed) * transform.right.x;
+        tempMove.z = (moveInput.y * speed) * transform.forward.z;
+
+        rb.linearVelocity = tempMove;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void Jump()
+    {
+        if(Physics.Raycast(jumpRay, jumpDetectDistance))
+        {
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+        }
     }
 }
