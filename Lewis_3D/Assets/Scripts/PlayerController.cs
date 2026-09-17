70a4f03973647ed944e9df01fb7958d1585190f2
Ian Lewis <33504743+wellthathappened@users.noreply.github.com>
2026-09-17 10:22:57 -0700

    Began weapon system for 3D

--- Lewis_3D/Assets/Scripts/PlayerController.cs
+++ Lewis_3D/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool isAttacking = false;
+
     public float speed = 5.0f;
     public float jumpHeight = 10f;
     public float jumpDetectDistance = 1.1f;
+    public float interactDistance = 6f;
 
     CinemachinePositionComposer cineCam;
     Camera playerCam;
     PlayerInput playerInput;
     Rigidbody rb;
 
+    public Weapon currentWeapon;
+    public Transform weaponSlot;
+    public GameObject pickupObj;
+
     Ray jumpRay;
+    Ray interactRay;
+    RaycastHit interactHit;
     Vector2 moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,9 +31,6 @@ public class PlayerController : MonoBehaviour
         // Initializing component data
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
-
-        
-
         playerCam = Camera.main;
         cineCam = GameObject.Find("CinemachineCamera").GetComponent<CinemachinePositionComposer>();
 
@@ -32,6 +38,9 @@ public class PlayerController : MonoBehaviour
         moveInput = Vector2.zero;
 
         jumpRay = new Ray(transform.position, -transform.up);
+        interactRay = new Ray(playerCam.transform.position, playerCam.transform.forward);
+
+        weaponSlot = transform.GetChild(0);
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -50,6 +59,23 @@ public class PlayerController : MonoBehaviour
     {
         jumpRay.origin = transform.position;
         jumpRay.direction = -transform.up;
+
+        interactRay.origin = playerCam.transform.position;
+        interactRay.direction = playerCam.transform.forward;
+
+        if (Physics.Raycast(interactRay, out interactHit, interactDistance))
+        {
+            if (interactHit.collider.tag == "Weapon")
+            {
+                pickupObj = interactHit.collider.gameObject;
+            }
+        }
+        else
+            pickupObj = null;
+
+        if (currentWeapon)
+            if (currentWeapon.holdToAttack && isAttacking)
+                currentWeapon.fire();
 
         Vector3 tempMove = rb.linearVelocity;
 
@@ -77,5 +103,39 @@ public class PlayerController : MonoBehaviour
     public void shoulderSwap()
     {
         cineCam.TargetOffset.x *= -1;
+    }
+
+    public void Reload()
+    {
+        if (currentWeapon)
+            if (!currentWeapon.reloading)
+                currentWeapon.reload();
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if(currentWeapon)
+        {
+            if (currentWeapon.holdToAttack)
+            {
+                if (context.ReadValueAsButton())
+                    isAttacking = true;
+                else
+                    isAttacking = false;
+            }
+
+            else if (context.ReadValueAsButton())
+                currentWeapon.fire();
+        }
+    }
+
+    public void Interact()
+    {
+
+    }
+
+    public void DropWeapon()
+    {
+
     }
 }
---
+++ Lewis_3D/Assets/Scripts/Weapon.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    PlayerController player;
+
+    public GameObject projectile;
+    public Transform firePoint;
+    public Camera firingDirection;
+
+    [Header("Meta Attributes")]
+    public bool canFire = true;
+    public bool holdToAttack = true;
+    public bool reloading = false;
+    public int weaponID;
+    public string weaponName;
+
+    [Header("Weapon Stats")]
+    public float projLifespan;
+    public float projVelocity;
+    public float reloadCooldown;
+    public float rof;
+    public int fireModes;
+    public int currentFiremode;
+    public int clip;
+    public int clipSize;
+
+    [Header("Ammo Stats")]
+    public int ammo;
+    public int maxAmmo;
+    public int ammoRefill;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        firePoint = transform.GetChild(0);
+        firingDirection = Camera.main;
+    }
+
+    public void equip()
+    {
+        player.currentWeapon = this;
+
+        transform.SetPositionAndRotation(player.weaponSlot.position, player.weaponSlot.rotation);
+        transform.SetParent(player.weaponSlot);
+
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    public void unequip()
+    {
+        player.currentWeapon = null;
+
+        transform.SetParent(null);
+
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Collider>().isTrigger = false;
+
+        this.player = null;
+    }
+
+    public void fire()
+    {
+        if(canFire && !reloading && clip > 0)
+        {
+            GameObject p = Instantiate(projectile, firePoint.position, firePoint.rotation);
+            p.GetComponent<Rigidbody>().AddForce(firingDirection.transform.forward * projVelocity);
+            Destroy(p, projLifespan);
+            canFire = false;
+            clip--;
+            StartCoroutine("cooldownFire");
+        }
+    }
+
+    public void reload()
+    {
+        if (clip >= clipSize)
+            return;
+
+        reloading = true;
+        canFire = false;
+
+        int reloadCount = clipSize - clip;
+
+        if (ammo < reloadCount)
+        {
+            clip += ammo;
+            ammo = 0;
+        }
+
+        else
+        {
+            clip += reloadCount;
+            ammo -= reloadCount;
+        }
+
+        StartCoroutine("reloadingCooldown");
+    }
+
+    public void recoil()
+    {
+
+    }
+
+    IEnumerator cooldownFire()
+    {
+        yield return new WaitForSeconds(rof);
+
+        if(clip > 0)
+            canFire = true;
+    }
+
+    IEnumerator reloadingCooldown()
+    {
+        yield return new WaitForSeconds(reloadCooldown);
+
+        reloading = false;
+        canFire = true;
+    }
+
+    /*
+    IEnumerator burstDuration()
+    {
+
+    }
+    */
+}
